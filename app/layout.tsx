89a52d56f092c21e82d3694d020fbf1cc4aa6c24
venomblaze-alpha <venomblaze.alpha@gmail.com>
2025-04-13 05:31:22 +0500

    changed the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/fav.png" type="image/png" />
+      </head>
       <body className={dmSans.className}>
         <main className="max-w-7xl mx-auto bg-[#17110f]">
           <Hero />
